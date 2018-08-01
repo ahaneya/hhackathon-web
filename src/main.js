adca98d1d9f46cce91498a88109cd8bea786c1a2
Anas Haneya <a.haneya@gmail.com>
2018-08-01 14:43:27 +0300

    adding basic components
    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import BootstrapJs from '../node_modules/bootstrap/dist/js/bootstrap.min';
 Vue.config.productionTip = false
 
 new Vue({
